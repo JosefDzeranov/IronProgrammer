3fc9dd7c9fecc0b51a2c3028c976652202d43b94
Josef Dzeranov <soslan29@gmail.com>
2017-09-01 16:06:43 +0300

    Update user profile

--- IronProgrammer/IronProgrammer/Models/EF/UserProfile.cs
+++ IronProgrammer/IronProgrammer/Models/EF/UserProfile.cs
@@ -10,43 +10,9 @@ namespace IronProgrammer.Models.EF
         [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
 
-        public string UserName { get; set; }
         public string Email { get; set; }
 
-        [DefaultValue(null)]
-        public DateTime? LastAccessTime { get; set; }
-
-        public string FirstName { get; set; }
+        public string FistName { get; set; }
         public string SecondName { get; set; }
-        public string ThirdName { get; set; }
-
-        [DefaultValue(null)]
-        public DateTime? BirthDay { get; set; }
-
-        public string PhoneNumber { get; set; }
-
-        public int? GradeLevel { get; set; } // Year of study
-
-        [ForeignKey("UserCategory")]
-        public int Category { get; set; }
-        public virtual UserCategory UserCategory { get; set; }
-
-        [ForeignKey("Country")]
-        public int? CountryID { get; set; }
-        public Country Country { get; set; }
-
-        [ForeignKey("City")]
-        public int? CityID { get; set; }
-        public City City { get; set; }
-
-        [ForeignKey("Institution")]
-        public int? InstitutionID { get; set; }
-        public Institution Institution { get; set; }
-
-        [ForeignKey("CreatedByUser")]
-        public int? CreatedByUserID { get; set; }
-        public virtual UserProfile CreatedByUser { get; set; }
-
-        public int? Generated { get; set; }
     }
 }
