d8ed0f2e652a2913a4e99dda6336e90dbf0142b8
idzeranov <idzeranov@MSK-SITDIKOV>
2017-09-21 16:12:56 +0300

    Add new project for testing Roslyn

---
+++ RoslynTest/RoslynTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Linq;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace ConstructionCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceCode = @"int a = Convert.ToInt16(Console.ReadLine());
+            int b = Convert.ToInt16(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine(b);
+            }
+            ";
+
+            //or parsefile etc
+            var syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
+
+            string[] identifierNames = syntaxTree.GetRoot().DescendantNodes()
+                .OfType<VariableDeclaratorSyntax>().Select(v => v.Identifier.Text)
+                .Concat(syntaxTree.GetRoot().DescendantNodes().OfType<ParameterSyntax>().Select(p => p.Identifier.Text))
+                .ToArray();
+
+            foreach (var VARIABLE in identifierNames)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+        }
+
+    }
+
+}
