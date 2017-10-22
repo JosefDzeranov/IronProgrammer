ffa7f6a15a7c8aa6c6e2072723530f5cd6d954cd
Josef Dzeranov <soslan29@gmail.com>
2017-10-22 14:14:43 +0300

    Add business layer

---
+++ IronProgrammer/IronProgrammer.Infrastructure.Business/TracingSolverProblem.cs
@@ -0,0 +1,18 @@
+﻿using IronProgrammer.Domain.Core.EF;
+using IronProgrammer.Services.Interfaces;
+
+namespace IronProgrammer.Infrastructure.Business
+{
+    public class TracingSolverProblem : ISolverProblem
+    {
+        public void BuildTask(Problem problem)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void PostTask(Problem problem)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- IronProgrammer/IronProgrammer.Services.Interfaces/ISolverProblem.cs
+++ IronProgrammer/IronProgrammer.Services.Interfaces/ISolverProblem.cs
@@ -1,7 +1,11 @@
-﻿namespace IronProgrammer.Services.Interfaces
-{
-    interface ISolverProblem
-    {
+﻿using System.Reflection.Emit;
+using IronProgrammer.Domain.Core.EF;
 
+namespace IronProgrammer.Services.Interfaces
+{
+    public interface ISolverProblem
+    {
+        void BuildTask(Problem problem);
+        void PostTask(Problem problem);
     }
 }
