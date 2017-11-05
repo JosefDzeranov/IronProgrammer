8f534d9da2494ebf05906dcec7d8d3b94d86f206
Josef Dzeranov <soslan29@gmail.com>
2017-11-05 21:35:43 +0300

    add RoslynCompiler

---
+++ IronProgrammer/IronProgrammer.Services.Compile/RoslynCompiler.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using IronProgrammer.Services.Interfaces;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace IronProgrammer.Services.Compile
+{
+    /// <summary>
+    /// Compiler that uses the Roslyn compiler (<see cref="CSharpCompilation" />).
+    /// </summary>
+    public class RoslynCompiler : ICompiler
+    {
+        private const string path = @"D:\\Compilers";
+
+
+        /// <summary>
+        /// Gets the compilation options.
+        /// </summary>
+        /// <value>
+        /// The options.
+        /// </value>
+        public CSharpCompilationOptions Options { get; } = new CSharpCompilationOptions(
+            OutputKind.ConsoleApplication,
+            reportSuppressedDiagnostics: true,
+            optimizationLevel: OptimizationLevel.Release,
+            generalDiagnosticOption: ReportDiagnostic.Error
+        );
+
+        /// <summary>
+        /// Compiles the specified code the sepcified assembly locations.
+        /// </summary>
+        /// <param name="code">The code.</param>
+        /// </returns>
+        /// <param name="assemblyLocations">The assembly locations.</param>
+        /// <returns>
+        /// The assembly.
+        /// <exception cref="RoslynCompilationException">Assembly could not be created.</exception>
+        public Assembly Compile(string code, string exeName, params string[] assemblyLocations)
+        {
+            string netAssembliesDirectory = Path.GetDirectoryName(typeof(object).Assembly.Location);
+            var references = assemblyLocations.Select(l => MetadataReference.CreateFromFile(netAssembliesDirectory + "\\" + l));
+
+            var compilation = CSharpCompilation.Create(
+                exeName,
+                references: references,
+                syntaxTrees: new SyntaxTree[] { CSharpSyntaxTree.ParseText(code) },
+                options: this.Options
+            );
+
+            using (var ms = new MemoryStream())
+            {
+                var compilationResult = compilation.Emit(ms);
+
+                if (compilationResult.Success)
+                {
+                    ms.Seek(0, SeekOrigin.Begin);
+                    return Assembly.Load(ms.ToArray());
+                }
+
+                throw new RoslynCompilationException("Assembly could not be created.", compilationResult);
+            }
+        }
+    }
+}
--- IronProgrammer/IronProgrammer/Controllers/HomeController.cs
+++ IronProgrammer/IronProgrammer/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ namespace IronProgrammer.Controllers
 {
     public class HomeController : Controller
     {
-        private ICompiler _compiler = new CodeDomCompiler();
+        private ICompiler _compiler = new RoslynCompiler();
 
         public ActionResult Index()
         {
@@ -23,7 +23,7 @@ namespace IronProgrammer.Controllers
         [HttpPost]
         public ActionResult CodeWrite(string code)
         {
-            var a = _compiler.Compile(code, "Josef", "System.dll");
+            var a = _compiler.Compile(code, "Josef.exe", "mscorlib.dll","System.Core.dll");
             Process.Start("D:\\Compilers\\Josef.exe");
             return View();
         }
